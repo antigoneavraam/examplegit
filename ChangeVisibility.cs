1c92ff64a8a44c5e7e5a9d178e372c168a01268a
ThemisP <themispapathemistocleous@gmail.com>
2020-08-08 18:06:53 +0300

    New unity files

---
+++ ChangeVisibility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeVisibility : MonoBehaviour
+{
+    MeshRenderer renderer;
+    bool visibility = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("t")){
+            visibility = !visibility;
+            renderer.enabled = visibility;
+        }
+    }
+}
---
+++ NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int scale = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello scale:" + scale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("w")){
+            this.transform.position += new Vector3(0,1,0);
+        }
+        Debug.Log(scale);
+    }
+}
